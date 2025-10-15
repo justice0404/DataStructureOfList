5734d22174037b44b79f4f5b7139ad573a8fe53b
justice0404 <wzw13579@qq.com>
2025-10-15 23:27:02 +0800

    up beta again

--- SqList/hello.c
+++ SqList/hello.c
@@ -5,5 +5,6 @@ int main(int argc, char const *argv[])
     printf("Hello World!\r\n");
     printf("Hello Git!\r\n");
     printf("The new method of Up Branch\r\n");
+    printf("The same method Up Branch again\r\n");
     return 0;
 }
