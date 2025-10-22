d8597c290cae17898031c7b3805a89413113bd09
justice0404 <wzw13579@qq.com>
2025-10-22 18:25:33 +0800

    LinkList的基本定义

---
+++ Inc/LinkList.h
@@ -0,0 +1,35 @@
+#ifndef __LINKLIST_H
+
+#define __LINKLIST_H
+
+#define ElemType int
+
+#include <iostream>
+
+typedef struct LNode
+{
+    /* data */
+    ElemType data;
+    LNode * next;
+}LNode,*LinkList;
+
+bool InitList(LinkList &L);
+bool ListInsert(LinkList &L,int i,ElemType e);
+bool ListDelete(LinkList &L,int i,ElemType &e);
+LNode* GetElem(LinkList L,int i);
+LNode* LocateElem(LinkList L,ElemType e);
+int Length(LinkList L);
+bool IsEmpty(LinkList L){ return (L->next == NULL); }
+
+bool InsertNextNode(LNode* p,ElemType e);
+bool InsertPriorNode(LNode* p,ElemType e);
+bool DeleteNode(LNode *p);
+
+LinkList ListTailInsert(LinkList &L);
+LinkList ListHeadInsert(LinkList &L);
+
+void ReverseList(LinkList &L);
+
+void test02(void);
+
+#endif
---
+++ Src/LinkList.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include "LinkList.h"
+
+using namespace std;
+
+//初始化一个带头结点的单链表
+bool InitList(LinkList &L)
+{
+    //分配一个地址给头结点
+    L = (LNode*)malloc(sizeof(LNode));
+    if(L == NULL) return false;
+    //头结点指向NULL
+    L->next = NULL;
+    return true;
+}
+
+//在链表L的第i个位置插入元素e
+bool ListInsert(LinkList &L,int i,ElemType e)
+{
+    if(i<1)
+    {
+        cout << "Invalid position input!" << endl;
+        return false;
+    }
+
+   
+    /*这里的指针移动最好画出来
+    *   p一开始和L一样指向头节点,j=0；假设在第三个节点插入，当j自增到2之后，下个迭代将会停止。
+    *   L -> 头 -> 1 -> 2 -> 3 -> 4 -> NULL
+    *       j=0   j=1  j=2
+    */
+    LNode *p;   //指示当前扫描的节点
+    p = L;      //将p指向头结点
+    int j=0;    //指示当前为第几个节点
+    while (p!=NULL && j<i-1)
+    {
+        p = p->next;
+        j++;
+    }
+    if(p == NULL)
+    {
+        cout << "Invalid position input!" << endl;
+        return false;
+    }
+    
+    LNode* s = (LNode*)malloc(sizeof(LNode));
+    s->data = e;
+    s->next = p->next;  //先连
+    p->next = s;        //后断
+
+    return true;
+}
+
+//在链表L的第i个位置删除节点，并用e返回
+bool ListDelete(LinkList &L,int i,ElemType &e)
+{
+    if(i<1)
+    {
+        cout << "Invalid position input!" << endl;
+        return false;
+    }
+
+    LNode* p = (LNode*)malloc(sizeof(LNode));
+    p = L;
+    int j = 0;
+    while(p != NULL && j<i-1)
+    {
+        p = p->next;
+        j++;
+    }
+    if(p == NULL)
+    {
+        cout << "Invalid position input!" << endl;
+        return false;
+    }
+    if(p->next == NULL)    //要删除的节点不存在
+    {
+        cout << "Invalid position input!" << endl;
+        return false;
+    }
+
+    LNode * q = p->next;
+    e = q->data;
+    p->next = q->next;  //断开
+    free(q);
+
+    return true;
+}
+
